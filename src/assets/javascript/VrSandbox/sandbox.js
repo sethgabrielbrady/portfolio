190f5943c1a158b03df0fcbe5b44666e00c491ca
sethgabrielbrady <sethgabrielbrady@gmail.com>
2024-07-18 17:30:52 -0400

    adds paddle control

--- src/assets/javascript/VrSandbox/sandbox.js
+++ src/assets/javascript/VrSandbox/sandbox.js
@@ -2,11 +2,13 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { World } from 'three/examples/jsm//libs/ecsy.module.js';
 import { createText } from 'three/examples/jsm/webxr/Text2D.js';
-// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+const raycaster = new THREE.Raycaster();
 
 function sandbox() {
   const world = new World();
   const clock = new THREE.Clock();
+  let delta = 0;
   let camera, renderer, scene;
   let backgroundColor = 0x222222;
 
@@ -23,7 +25,6 @@ function sandbox() {
   animate();
   //units a second
   let speed = 0.2;
-  let delta = 1/60;
 
   function init() {
     scene = new THREE.Scene();
@@ -32,9 +33,12 @@ function sandbox() {
     const aspect = (window.innerWidth / window.innerHeight);
     const distance = 7;
     camera = new THREE.OrthographicCamera(- distance * aspect, distance * aspect, distance, - distance, 1, 1000);
+
+
     camera.position.set( 20, 20, 20 ); // all components equal
-    // camera.position.set( 1.4788384930664669e-8, 34.641016151360226, 0.00003464255276393731 );
     camera.lookAt( scene.position ); // or the origin
+
+    console.log("camera", scene.position);
 
     const light = new THREE.DirectionalLight( 0xffffff, 3 );
     light.position.set( 0, 6, 0 );
@@ -50,6 +54,7 @@ function sandbox() {
     } );
 
 
+
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
     renderer.shadowMap.enabled = true;
@@ -59,69 +64,6 @@ function sandbox() {
 
     renderer.xr.enabled = true;
     renderer.xr.cameraAutoUpdate = false;
-
-    //  Grid
-    //  const gridHelper = new THREE.GridHelper(100, 100, 0x18fbe3,0x18fbe3);
-    //  scene.add( gridHelper );
-    //  gridHelper.visible = false;
-
-    //  window.addEventListener( 'keydown', ( event ) => {
-    //   if (event.key === 'g') {
-    //     gridHelper.visible = !gridHelper.visible;
-    //   }
-    //  });
-
-
-     // axis helper
-    // const axesHelper = new THREE.AxesHelper( 5 );
-    // scene.add( axesHelper );
-    // const xText = createText( 'x', 1 );
-    // const xText2 = createText( '-x', 1 );
-    // const zText = createText( "z", 1 );
-    // const zText2 = createText( "-z", 1 );
-    // const axisGroup = new THREE.Group();
-
-    // // update axis text position
-    // xText.position.set( 5, 1, 0 );
-    // xText2.position.set( -5, 1, 0 );
-    // zText.position.set( 0, 1, 5 );
-    // zText2.position.set( 0, 1, -5 );
-
-    // // update axis text rotation
-    // xText.rotation.x = Math.PI / 2;
-    // xText2.rotation.x = Math.PI / 2;
-    // zText.rotation.x = Math.PI / 2;
-    // zText2.rotation.x = Math.PI / 2;
-
-    // axisGroup.add(xText, zText, xText2, zText2);
-
-    // scene.add(axisGroup);
-
-
-    // models
-    // function loadModel (modelObj) {
-    //   let gltfLoader = new GLTFLoader();
-    //   gltfLoader.load(modelObj.path,
-    //     (gltf) => {
-    //      let model = gltf.scene
-    //       model.scale.x = modelObj.scale;
-    //       model.scale.y = modelObj.scale;
-    //       model.scale.z = modelObj.scale;
-    //       model.position.x = modelObj.position.x;
-    //       model.position.y = modelObj.position.y;
-    //       model.position.z = modelObj.position.z;
-    //       model.castShadow = true;
-    //       scene.add(model);
-    //     }
-    //   )
-    // }
-
-    // const palm = {
-    //   scale: 0.045,
-    //   path: 'models/palmshiny.glb',
-    //   position: { x: -4, y: 0, z: 0 }
-    // }
-    // loadModel(palm);
 
     //orbit controls
     let orbitEnabled = false;
@@ -136,7 +78,67 @@ function sandbox() {
         orbitEnabled = !orbitEnabled
         orbitControls.enableRotate = orbitEnabled;
       }
+      if (event.key === 'b') {
+        if (!ballInPlay) {
+          addBall();
+        }
+      }
+      if (event.key === 'c') {
+        cameraSwitch = !cameraSwitch;
+        translateCamera();
+      }
      });
+
+     window.addEventListener( 'click', ( event ) => {
+      if (!ballInPlay) {
+          addBall();
+        }
+      });
+
+
+
+        // // axis helper
+    const axesHelper = new THREE.AxesHelper( 5 );
+    scene.add( axesHelper );
+    const xText = createText( 'x', 1 );
+    const xText2 = createText( '-x', 1 );
+    const zText = createText( "z", 1 );
+    const zText2 = createText( "-z", 1 );
+    const axisGroup = new THREE.Group();
+
+    // update axis text position
+    xText.position.set( 5, 1, 0 );
+    xText2.position.set( -5, 1, 0 );
+    zText.position.set( 0, 1, 5 );
+    zText2.position.set( 0, 1, -5 );
+
+    // update axis text rotation
+    xText.rotation.x = Math.PI / 2;
+    xText2.rotation.x = Math.PI / 2;
+    zText.rotation.x = Math.PI / 2;
+    zText2.rotation.x = Math.PI / 2;
+    axisGroup.add(xText, zText, xText2, zText2);
+    scene.add(axisGroup);
+
+    let cameraSwitch = false;
+
+
+    function addGrid() {
+      const grid = new THREE.GridHelper( 10, 10, 0xff0000, 0x00ff00 );
+      grid.material.opacity = 0.5;
+      grid.material.transparent = true;
+      scene.add( grid );
+    }
+    addGrid();
+
+     function translateCamera() {
+      if (cameraSwitch) {
+        camera.position.set( 1.630175897290663e-7, 34.641016151360226, 0.00003464063257599432 );
+      } else {
+        camera.position.set( 20, 20, 20 );
+      }
+      camera.lookAt( scene.position )
+     }
 
 
     //floor
@@ -159,12 +161,12 @@ function sandbox() {
     scene.add(floor);
 
     window.addEventListener( 'resize', onWindowResize() );
+    container.addEventListener( 'pointermove', onIntersect );
   }
 
   //Update all the collision to use intersecting bounding boxes instead of the position of the objects
   //breakout
   const breakoutObject = new THREE.Group();
-  // const startingYPos = 18.84;
   const startingYPos = 0.25
 
 
@@ -174,11 +176,22 @@ function sandbox() {
   const wallAxis = 5.00;
   let xAxis = (speed * delta)
   let yAxis = -1 + (speed * delta)
+  let ballInPlay = false;
 
-  ball.position.x = randomizeDirection();
-  ball.position.y = startingYPos;
-  ball.position.z = -1;
-  breakoutObject.add(ball);
+
+
+  function addBall(x) {
+    ballInPlay = !ballInPlay;
+    ball.position.x = x || randomizeDirection();
+    ball.position.y = startingYPos;
+    ball.position.z = 4;
+    breakoutObject.add(ball);
+    xAxis = (speed * delta)
+    yAxis = -1 + (speed * delta)
+
+  }
+
+  addBall();
 
 
   const paddleGeo = new THREE.BoxGeometry( 2.0, .25, .25 );
@@ -187,7 +200,7 @@ function sandbox() {
 
   paddle.position.y = startingYPos;
   paddle.position.x = 0
-  paddle.position.z = 4.5;
+  paddle.position.z = 5;
   breakoutObject.add(paddle);
 
 
@@ -217,7 +230,7 @@ function sandbox() {
   //brick collisions
   function checkBrickCollision() {
     bricks.children.forEach((brick) => {
-      if (ball.position.x >= brick.position.x - 1 && ball.position.x <= brick.position.x + 1) {
+      if (ball.position.x >= brick.position.x - 0.5 && ball.position.x <= brick.position.x + 0.5) {
         if(ball.position.z >= brick.position.z - .25 && ball.position.z <= brick.position.z + .25) {
           bricks.remove(brick);
           yAxis = -yAxis + getRandomArbitrary();
@@ -227,19 +240,35 @@ function sandbox() {
     });
   }
 
+  function checkPaddeCollision() {
+    if (ball.position.x >= paddle.position.x - 1 && ball.position.x <= paddle.position.x + 1 ){
+      if (ball.position.z >= paddle.position.z - 0.35 && ball.position.z <= paddle.position.z + 0.35) {
+        yAxis = -yAxis + getRandomArbitrary();
+        xAxis = (-xAxis + getRandomArbitrary()) * randomizeDirection();
+      }
+    }
+  }
+
   function collisions(){
+    if (xAxis >= 2) {
+      xAxis = 2;
+    }
+    if (yAxis >= 2) {
+      yAxis = 2;
+    }
+    // console.log("xAxis", xAxis, yAxis);
     let smoothness = .175;
     ball.translateX( xAxis * smoothness);
     ball.translateZ( yAxis * smoothness);
 
     if(ball.position.x <= -1 * wallAxis  ) {
       ball.position.x = (-1 * wallAxis)
-      xAxis = -xAxis + getRandomArbitrary();
+      xAxis = (-1 * (xAxis + getRandomArbitrary()))* randomizeDirection();
     }
 
     if(ball.position.x >= wallAxis) {
       ball.position.x = wallAxis
-      xAxis = -xAxis + getRandomArbitrary();
+      xAxis = (-1 * (xAxis + getRandomArbitrary()))* randomizeDirection();
     }
 
     if(ball.position.z <= -1 * wallAxis) {
@@ -247,17 +276,13 @@ function sandbox() {
       yAxis = -yAxis + getRandomArbitrary();
     }
 
-    if(ball.position.z >= wallAxis) {
-      ball.position.z = wallAxis
-      yAxis = -yAxis + getRandomArbitrary();
+
+    if(ball.position.z >= wallAxis + 2) {
+     breakoutObject.remove(ball);
+     ballInPlay = !ballInPlay;
     }
 
-    if (ball.position.x >= paddle.position.x - 2 && ball.position.x <= paddle.position.x + 2) {
-      if(ball.position.z >= paddle.position.z - .25 && ball.position.z <= paddle.position.z + .25) {
-        yAxis = -yAxis + getRandomArbitrary();
-        xAxis = -xAxis + getRandomArbitrary();
-      }
-    }
+    checkPaddeCollision();
     checkBrickCollision();
   }
 
@@ -276,49 +301,28 @@ function sandbox() {
     renderer.setAnimationLoop( render );
   }
 
-  // function updateBreakoutYPosition() {
-  //   breakoutObject.translateY(-0.1);
-  //   console.log(breakoutObject.position.y);
-  // }
+  const pointer = new THREE.Vector2();
 
-  // function updateCameraPosition() {
-  //   let cx = camera.position.x;
-  //   let cy = camera.position.y;
-  //   let cz = camera.position.z;
-  //   if (camera.position.y > 20 ) {
-  //     cy -= 0.1;
-  //   }
-  //   if (camera.position.x < 20 ) {
-  //     cx  += 0.1;
-  //   }
-  //   if (camera.position.z < 20 ) {
-  //     cz  += 0.1;
-  //   }
-  //   camera.position.set(cx, cy, cz);
-  // }
+  function onIntersect( event ) {
+    pointer.x = event.clientX / 90 ;
+    raycaster.setFromCamera( pointer, camera );
+		// See if the ray from the camera into the world hits one of our meshes
+		// const intersects = raycaster.intersectObject( paddle );
+    // console.log("paddle", intersects.length);
+
+    if (pointer.x > 5 && pointer.x < 15) {
+      paddle.position.x = pointer.x - 10;
+    }
+  }
 
   function render() {
-    let delta = clock.getDelta();
+    delta = clock.getDelta();
     const elapsedTime = clock.elapsedTime;
     renderer.xr.updateCamera( camera );
     world.execute( delta, elapsedTime );
     renderer.render( scene, camera );
     collisions();
-
-    // if (elapsedTime > 3.0 && elapsedTime < 6.1) {
-    //   updateBreakoutYPosition();
-    // }
-    // if (elapsedTime > 1.0){
-    //   updateCameraPosition();
-    //   }
-    // }
-    // if (camera.position.y == 20 && camera.position.x == 20 && camera.position.z == 20) {
-    //   camera.update
-    // }
-
   }
 }
 
-//{x: 1.4788384930664669e-8, y: 34.641016151360226, z: 0.00003464255276393731}
-// position{x: 20, y: 20, z: 20.000000000000004}
 export { sandbox };
--- src/assets/javascript/other/codebits.ts
+++ src/assets/javascript/other/codebits.ts
@@ -30,3 +30,96 @@
     //   return parseInt(newColor, 16);
     // }
 
+
+
+    // function updateBreakoutYPosition() {
+  //   breakoutObject.translateY(-0.1);
+  //   console.log(breakoutObject.position.y);
+  // }
+
+  // function updateCameraPosition() {
+  //   let cx = camera.position.x;
+  //   let cy = camera.position.y;
+  //   let cz = camera.position.z;
+  //   if (camera.position.y > 20 ) {
+  //     cy -= 0.1;
+  //   }
+  //   if (camera.position.x < 20 ) {
+  //     cx  += 0.1;
+  //   }
+  //   if (camera.position.z < 20 ) {
+  //     cz  += 0.1;
+  //   }
+  //   camera.position.set(cx, cy, cz);
+  // }
+
+  //camera movement base on time
+
+      // if (elapsedTime > 3.0 && elapsedTime < 6.1) {
+    //   updateBreakoutYPosition();
+    // }
+    // if (elapsedTime > 1.0){
+    //   updateCameraPosition();
+    //   }
+    // }
+    // if (camera.position.y == 20 && camera.position.x == 20 && camera.position.z == 20) {
+    //   camera.update
+    // }
+
+
+
+//model loader
+    // models
+    // function loadModel (modelObj) {
+    //   let gltfLoader = new GLTFLoader();
+    //   gltfLoader.load(modelObj.path,
+    //     (gltf) => {
+    //      let model = gltf.scene
+    //       model.scale.x = modelObj.scale;
+    //       model.scale.y = modelObj.scale;
+    //       model.scale.z = modelObj.scale;
+    //       model.position.x = modelObj.position.x;
+    //       model.position.y = modelObj.position.y;
+    //       model.position.z = modelObj.position.z;
+    //       model.castShadow = true;
+    //       scene.add(model);
+    //     }
+    //   )
+    // }
+
+    // const palm = {
+    //   scale: 0.045,
+    //   path: 'models/palmshiny.glb',
+    //   position: { x: -4, y: 0, z: 0 }
+    // }
+    // loadModel(palm);
+    //model loader
+
+
+//axes helper
+    // // axis helper
+    // const axesHelper = new THREE.AxesHelper( 5 );
+    // scene.add( axesHelper );
+    // const xText = createText( 'x', 1 );
+    // const xText2 = createText( '-x', 1 );
+    // const zText = createText( "z", 1 );
+    // const zText2 = createText( "-z", 1 );
+    // const axisGroup = new THREE.Group();
+
+    // // update axis text position
+    // xText.position.set( 5, 1, 0 );
+    // xText2.position.set( -5, 1, 0 );
+    // zText.position.set( 0, 1, 5 );
+    // zText2.position.set( 0, 1, -5 );
+
+    // // update axis text rotation
+    // xText.rotation.x = Math.PI / 2;
+    // xText2.rotation.x = Math.PI / 2;
+    // zText.rotation.x = Math.PI / 2;
+    // zText2.rotation.x = Math.PI / 2;
+
+    // axisGroup.add(xText, zText, xText2, zText2);
+
+    // scene.add(axisGroup);
+
+  //a axes helper
